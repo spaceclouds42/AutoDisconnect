b899404f29570728a35d8d79b5b0fd185f519fcc
SpaceClouds42 <spaceclouds42@gmail.com>
2021-01-12 21:10:40 -0500

    forgot to remove event stuff from this file ._.

--- src/main/java/autodisconnect/mixin/ClientWorldMixin.java
+++ src/main/java/autodisconnect/mixin/ClientWorldMixin.java
@@ -1,6 +1,5 @@
 package autodisconnect.mixin;
 
-import autodisconnect.events.Events;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -9,14 +8,12 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import static autodisconnect.Globals.EVENT_BUS;
-
 @Mixin(ClientWorld.class)
 abstract public class ClientWorldMixin {
     @Inject(method = "addEntityPrivate", at = @At("TAIL"))
     private void addEntityPrivate(int id, Entity entity, CallbackInfo ci) {
         if (entity instanceof PlayerEntity) {
-            EVENT_BUS.publish(Events.PLAYER_RENDERED, entity);
+            System.out.println("OH MY GOD THERE's A PLAYER!!!!!! aaaaaaahhhhhhhhh");
         }
     }
 }
